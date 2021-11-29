b39324c57899aa8a97374fc02f0bd35b1451e46c
Marcela Ismael <marcela.ismsilva@outlook.com>
2021-11-29 16:34:50 -0300

    atividade react1

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,11 +1,17 @@
-import './App.css'
-import Card from './components/Card'
+import './App.css';
+import Card from './components/Card';
+import './components/Titulo'
+import Titulo from './components/Titulo';
+import Imagem from './components/Imagem';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Card />
+        <p>Projeto React | Marcela Ismael</p>
+        <Titulo text="O que eu sempre escuto 🎶" />
+        <Card/>
+        <Imagem/>
       </header>
     </div>
   );
